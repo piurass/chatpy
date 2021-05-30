dbc0588c493d654d136cd6d1f110f3394b32c3a7
Mateus <piurass@yahoo.com.br>
2021-05-30 11:22:23 -0300

    progarma 1,00

---
+++ ola/ola.py
@@ -0,0 +1,12 @@
+resposta="ola"
+resposta2="eu sou o mateus"
+resposta3="apertando no intalar"
+resposta4="denada"
+print("ola")
+print(resposta)
+print("quem és tu")
+print(resposta2)
+print("como eu intalo jogo")
+print(resposta3)
+print("obrigado")
+print(resposta4)
