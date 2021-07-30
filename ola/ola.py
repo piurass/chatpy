5f2f8c87422a6e6d316ec78bc96cd1a0b133c4dc
Rogerio de Alencar Santos <rogerio.alencar@ibm.com>
2021-07-30 00:15:18 +0000

    desenvolvimento dentro do container

--- ola/ola.py
+++ ola/ola.py
@@ -2,6 +2,7 @@ resposta="ola"
 resposta2="eu sou o mateus"
 resposta3="apertando no intalar"
 resposta4="denada"
+resposta5="até mais"
 print("ola")
 print(resposta)
 print("quem és tu")
@@ -10,3 +11,5 @@ print("como eu intalo jogo")
 print(resposta3)
 print("obrigado")
 print(resposta4)
+print("fim")
+print(resposta5)
